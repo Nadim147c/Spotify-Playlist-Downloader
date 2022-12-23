3b3e619ad6b5ddc8b435645307d932541bb9eda2
Ephemeral <90786729+Nadim147c@users.noreply.github.com>
2022-12-23 15:53:53 +0600

    fix: added caching

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ from unicodedata import name
 from spotipy.oauth2 import SpotifyClientCredentials
 from tkinter import Tk, filedialog
 from dotenv import load_dotenv
+from colorama import init, Fore as color
 import urllib
 import yt_dlp
 import spotipy
 import eyed3
 import time
+import json
 import os
+
+init()
 
 load_dotenv()
 
@@ -33,7 +37,7 @@ def get_simplified_track_info(track):
         "name": track["track"]["name"],
         "cover": track["track"]["album"]["images"][0]["url"],
         "album": track["track"]["album"]["name"],
-        "artists": ', '.join(artists)
+        "artists": ", ".join(artists),
     }
     return result
 
@@ -43,14 +47,22 @@ def get_tracks(PLAYLIST_LINK: str):
     tracks = []
     playlist_uri = get_playlist_uri(PLAYLIST_LINK)
 
-    playlist = SP.playlist_tracks(playlist_uri)
-    name: str = SP.playlist(playlist_uri, fields="name")["name"]
-    for track in playlist["items"]:
-        result = get_simplified_track_info(track)
-        tracks.append(result)
+    try:
+        with open(f"cache/playlist/{playlist_uri}.json", "rb") as f:
+            tracks, playlist = json.loads(f.read())
+            print("cached playlist")
+    except:
+        playlist = SP.playlist_tracks(playlist_uri)
+        name: str = SP.playlist(playlist_uri, fields="name")["name"]
+        for track in playlist["items"]:
+            result = get_simplified_track_info(track)
+            tracks.append(result)
+
+        with open(f"cache/playlist/{playlist_uri}.json", "w", encoding="utf8") as f:
+            f.write(json.dumps([track, name], indent=4))
 
     amount = len(tracks)
-    print(f'Got total {amount} tracks')
+    print(f"Got total {amount} tracks")
 
     return tracks, name
 
@@ -63,22 +75,25 @@ def download_track(track, path):
     exists = os.path.exists(local_path)
 
     if exists:
-        return
+        return print("Already exists.")
 
     ydl_opts = {
         "outtmpl": "{}/{}.%(ext)s".format(path, name),
-        'format': 'bestaudio/best',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '320',
-        }],
+        "format": "bestaudio/best",
+        "postprocessors": [
+            {
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "mp3",
+                "preferredquality": "320",
+            }
+        ],
         "noplaylist": True,
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        url = ydl.extract_info(f"ytsearch:{name} lyrics", download=False, )[
-            'entries'][0]["webpage_url"]
+        url = ydl.extract_info(f"ytsearch:{name} lyrics", download=False,)["entries"][
+            0
+        ]["webpage_url"]
         ydl.download([url])
 
     print("Editing metadata.")
@@ -90,17 +105,27 @@ def download_track(track, path):
     audiofile.tag.title = name
     audiofile.tag.save()
 
-    cover = track['cover']
-    response = urllib.request.urlopen(cover)
-    imagedata = response.read()
-    audiofile.tag.images.set(3, imagedata, "image/jpeg", u"cover")
+    cover = track["cover"]
+    try:
+        file_name = os.path.split(cover)[1]
+        with open(f"cache/cover/{file_name}", "rb") as f:
+            image_data = f.read()
+        print("cached cover")
+    except:
+        image_data = urllib.request.urlopen(cover).read()
+        file_name = os.path.split(cover)[1]
+        with open(f"cache/cover/{file_name}", "wb") as f:
+            f.write(image_data)
+    audiofile.tag.images.set(3, image_data, "image/jpeg", "cover")
 
     audiofile.tag.save()
     time.sleep(20)
 
 
-url = input("Your Playlist URL:")
+mkdir = lambda n: (not os.path.exists(n)) and os.mkdir(n)
 
+
+url = input("Your Playlist URL:")
 
 tracks, name = get_tracks(url)
 
@@ -110,15 +135,22 @@ window = Tk()
 window.iconbitmap("icon.ico")
 window.withdraw()
 
-path = filedialog.askdirectory(
-    title="Select a location", initialdir="./", parent=window, mustexist=True) + "/" + name
+path = (
+    filedialog.askdirectory(
+        title="Select a location", initialdir="./", parent=window, mustexist=True
+    )
+    + "/"
+    + name
+)
 
 window.destroy()
 
-try:
-    os.mkdir(path)
-except OSError as err:
-    err
+mkdir(path)
+mkdir("cache")
+mkdir("cache/cover")
+mkdir("cache/playlist")
 
-for track in tracks:
+for i, track in enumerate(tracks):
+    title = track["name"]
+    print(f"\n\n{color.GREEN} { 1 + i }. { title } {color.RESET}\n")
     download_track(track, path)
